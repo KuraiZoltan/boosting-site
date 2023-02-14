4bef45bbe58b4181e4c78aba69291e194c925221
KuraiZoltan <kz1022@gmail.com>
2023-02-14 15:28:48 +0100

    Project created

---
+++ BoostingSite/Controllers/ApiController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BoostingSite.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ApiController : ControllerBase
+    {
+        private readonly ILogger<ApiController> _logger;
+
+        public ApiController(ILogger<ApiController> logger)
+        {
+            _logger = logger;
+        }
+    }
+}
