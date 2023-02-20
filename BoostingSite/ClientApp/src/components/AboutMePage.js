b5ce0a17da89f33571e1db13a95417a01d6eaf95
KuraiZoltan <kz1022@gmail.com>
2023-02-20 17:09:25 +0100

    About me page is added

---
+++ BoostingSite/ClientApp/src/components/AboutMePage.js
@@ -0,0 +1,24 @@
+﻿
+export function AboutMePage() {
+    const descriptionComponent = (
+        <div className="description-component">
+            <h2>Hi! My name is FakeBoy</h2>
+            <p>
+                asdasdrwe giuwreu jqioewra {'\n'}
+                q gertiu ghrqeiuog her q gerg {'\n'}
+                gerq ihuybqoer hgq rge ger fd {'\n'}
+                siudfhya uihnf eaw rtew {'\n'}
+            </p>
+            <a href="/">Home</a>
+        </div>
+        )
+
+    return (
+        <html>
+            <h1>About Me</h1>
+            <div>
+                {descriptionComponent}
+            </div>
+        </html>
+        )
+}
