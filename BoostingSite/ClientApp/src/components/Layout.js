fb6e93c041eeaa25101ef304e7ee48ed0f38dcad
KuraiZoltan <kz1022@gmail.com>
2023-02-20 17:10:41 +0100

    Layout update

--- BoostingSite/ClientApp/src/components/Layout.js
+++ BoostingSite/ClientApp/src/components/Layout.js
@@ -9,9 +9,9 @@ export class Layout extends Component {
     return (
       <div>
         <NavMenu />
-        <Container>
+        <div className="page-content">
           {this.props.children}
-        </Container>
+        </div>
       </div>
     );
   }
