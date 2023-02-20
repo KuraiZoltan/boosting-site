0ddb9a73256b4597596ea3753ae9f0d7d74a7cb1
KuraiZoltan <kz1022@gmail.com>
2023-02-20 17:13:11 +0100

    Home page html and basic mechanism addition

--- BoostingSite/ClientApp/src/components/Home.js
+++ BoostingSite/ClientApp/src/components/Home.js
@@ -1,26 +1,102 @@
-import React, { Component } from 'react';
+import React, { useState } from "react"
+import { UserDetailsComponent } from "./UserDetailsComponent"
 
-export class Home extends Component {
-  static displayName = Home.name;
+let selectRankFrom = ""
+let selectRankTo = ""
 
-  render() {
-    return (
-      <div>
-        <h1>Hello, world!</h1>
-        <p>Welcome to your new single-page application, built with:</p>
-        <ul>
-          <li><a href='https://get.asp.net/'>ASP.NET Core</a> and <a href='https://msdn.microsoft.com/en-us/library/67ef8sbd.aspx'>C#</a> for cross-platform server-side code</li>
-          <li><a href='https://facebook.github.io/react/'>React</a> for client-side code</li>
-          <li><a href='http://getbootstrap.com/'>Bootstrap</a> for layout and styling</li>
-        </ul>
-        <p>To help you get started, we have also set up:</p>
-        <ul>
-          <li><strong>Client-side navigation</strong>. For example, click <em>Counter</em> then <em>Back</em> to return here.</li>
-          <li><strong>Development server integration</strong>. In development mode, the development server from <code>create-react-app</code> runs in the background automatically, so your client-side resources are dynamically built on demand and the page refreshes when you modify any file.</li>
-          <li><strong>Efficient production builds</strong>. In production mode, development-time features are disabled, and your <code>dotnet publish</code> configuration produces minified, efficiently bundled JavaScript files.</li>
-        </ul>
-        <p>The <code>ClientApp</code> subdirectory is a standard React application based on the <code>create-react-app</code> template. If you open a command prompt in that directory, you can run <code>npm</code> commands such as <code>npm test</code> or <code>npm install</code>.</p>
-      </div>
-    );
-  }
+
+export function Home() {
+    const [dropdownOptionFromState, setDropdownOptionFromState] = useState("")
+    const [dropdownOptionToState, setDropdownOptionToState] = useState("")
+    function getSelectRankFrom(rank) {
+        selectRankFrom = rank
+        console.log(rank)
+    }
+
+    function getSelectRankTo(rank) {
+        selectRankTo = rank
+        console.log(rank)
+    }
+
+    const rankSelectFromBlock = (
+        <div className="select-from col-md-9">
+            <button onClick={() => getSelectRankFrom("Iron")}>Iron</button>
+            <button onClick={() => getSelectRankFrom("Bronze")}>Bronze</button>
+            <button onClick={() => getSelectRankFrom("Silver")}>Silver</button>
+            <button onClick={() => getSelectRankFrom("Gold")}>Gold</button>
+            <button onClick={() => getSelectRankFrom("Platinum") }>Platinum</button>
+            <button onClick={() => getSelectRankFrom("Diamond") }>Diamond</button>
+            <button onClick={() => getSelectRankFrom("Master") }>Master</button>
+            <button onClick={() => getSelectRankFrom("Grandmaster")}>Grandmaster</button>
+            <div>
+                <select onChange={(e) => { let selectedRank = e.target.value; setDropdownOptionFromState(selectedRank) }}>
+                    <option value="0">Nothing selected</option>
+                    <option value="4">IV</option>
+                    <option value="3">III</option>
+                    <option value="2">II</option>
+                    <option value="1">I</option>
+                </select>
+            </div>
+        </div>
+        
+    )
+    const rankSelectToBlock = (
+        <div className="select-to col-md-9">
+            <button onClick={() => getSelectRankTo("Iron") }>Iron</button>
+            <button onClick={() => getSelectRankTo("Bronze") }>Bronze</button>
+            <button onClick={() => getSelectRankTo("Silver") }>Silver</button>
+            <button onClick={() => getSelectRankTo("Gold") }>Gold</button>
+            <button onClick={() => getSelectRankTo("Platinum") }>Platinum</button>
+            <button onClick={() => getSelectRankTo("Diamond") }>Diamond</button>
+            <button onClick={() => getSelectRankTo("Master") }>Master</button>
+            <button onClick={() => getSelectRankTo("Grandmaster")}>Grandmaster</button>
+            <div>
+                <select onChange={(e) => { let selectedRank = e.target.value; setDropdownOptionToState(selectedRank) }}>
+                    <option value="0">Nothing selected</option>
+                    <option value="4">IV</option>
+                    <option value="3">III</option>
+                    <option value="2">II</option>
+                    <option value="1">I</option>
+                </select>
+            </div>
+        </div>
+        
+    )
+
+    const rankSelectionBlock = (
+        <div className="rank-selection-block">
+            {rankSelectFromBlock}
+            {rankSelectToBlock}
+        </div>
+        )
+
+    if (dropdownOptionFromState && dropdownOptionToState && selectRankFrom && selectRankTo) {
+        return (
+            <div className="home-page-content">
+                <div className="title-content">
+                    <h1>Boost your rank with FakeBoy!</h1>
+                </div>
+
+                <div className="order-details">
+                    {rankSelectionBlock}
+                    <div className="selectionResultBlock">
+                        <p>From {selectRankFrom} {dropdownOptionFromState} To {selectRankTo} {dropdownOptionToState}</p>
+                    </div>
+                    {UserDetailsComponent()}
+                </div>
+            </div>
+        )
+    } else {
+        return (
+            <div className="home-page-content">
+                <div className="title-content">
+                    <h1>Boost your rank with FakeBoy!</h1>
+                </div>
+                <div className="order-details">
+                    {rankSelectionBlock}
+                    {UserDetailsComponent()}
+                </div>
+            </div>
+            )
+    }
 }
