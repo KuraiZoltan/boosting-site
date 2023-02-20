86f14ee2d3d65d4b115040d4f3cdaa5b60e83c88
KuraiZoltan <kz1022@gmail.com>
2023-02-20 17:13:45 +0100

    User detail input addition

---
+++ BoostingSite/ClientApp/src/components/UserDetailsComponent.js
@@ -0,0 +1,25 @@
+﻿
+export function UserDetailsComponent() {
+    const inputFrom = (
+        <form className="input-block">
+            <label>
+                First Name:
+                <input type="text" name="name" required />
+            </label>
+            <label>
+                Last Name:
+                <input type="text" name="name" required />
+            </label>
+            <label>
+                Email:
+                <input type="email" name="name" required />
+            </label>
+        </form>
+        )
+    return (
+        <div className="input-details-container">
+            {inputFrom}
+        </div>
+        )
+}
+
