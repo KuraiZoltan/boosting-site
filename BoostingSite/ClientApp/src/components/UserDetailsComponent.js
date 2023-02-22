38ad237ac31af20884338a2370d1a8b0fe4b4445
KuraiZoltan <kz1022@gmail.com>
2023-02-22 14:56:59 +0100

    Input change handling and submit order button

--- BoostingSite/ClientApp/src/components/UserDetailsComponent.js
+++ BoostingSite/ClientApp/src/components/UserDetailsComponent.js
@@ -1,20 +1,72 @@
-﻿
-export function UserDetailsComponent() {
+﻿import React, { useState } from 'react';
+
+export function UserDetailsComponent(selectRankFrom, dropdownOptionFromState, selectRankTo, dropdownOptionToState) {
+    const [firstNameInputState, setFirstNameInputState] = useState("")
+    const [lastNameInputState, setLastNameInputState] = useState("")
+    const [emailInputState, setEmailInputState] = useState("")
+
+    function handleOnClick(selectRankFrom, dropDownFrom, selectRankTo, dropDownTo, firstName, lastName, email) {
+        let payload = {
+            CurrentRank: selectRankFrom,
+            CurrentRankLevel: dropDownFrom,
+            OrderedRank: selectRankTo,
+            OrderedRankLevel: dropDownTo,
+            FirstName: firstName,
+            LastName: lastName,
+            Email: email
+        }
+    }
+
+    const handleFirstNameOnChange = event => {
+        setFirstNameInputState(event.target.value)
+    }
+
+    const handleLastNameOnChange = event => {
+        setLastNameInputState(event.target.value)
+    }
+
+    const handleEmailOnChange = event => {
+        setEmailInputState(event.target.value)
+    }
+
+    const firstNameComponent = (
+        <label>
+            First Name:
+            <input type="text" name="firstName" onChange={handleFirstNameOnChange} required />
+        </label>
+    )
+
+    const lastNameComponent = (
+        <label>
+            Last Name:
+            <input type="text" name="lastName" onChange={handleLastNameOnChange} required />
+        </label>
+    )
+
+    const emailComponent = (
+        <label>
+            Email:
+            <input type="email" name="email" onChange={handleEmailOnChange} required />
+        </label>
+    )
+
     const inputFrom = (
-        <form className="input-block">
-            <label>
-                First Name:
-                <input type="text" name="name" required />
-            </label>
-            <label>
-                Last Name:
-                <input type="text" name="name" required />
-            </label>
-            <label>
-                Email:
-                <input type="email" name="name" required />
-            </label>
-        </form>
+        <div>
+            <form className="input-block">
+                {firstNameComponent}
+                {lastNameComponent}
+                {emailComponent}
+            </form>
+            <button id="submit-btn" onClick={() => handleOnClick(
+                selectRankFrom,
+                dropdownOptionFromState,
+                selectRankTo,
+                dropdownOptionToState,
+                firstNameInputState,
+                lastNameInputState,
+                emailInputState)}>Submit</button>
+        </div>
+        
         )
     return (
         <div className="input-details-container">
